71963850cbaaf7fff465962c50cc5e9a7ebf0105
tawseef- ahmad <tawseef.bhat@globallogic.com>
2022-12-10 20:44:42 +0530

    adding proto buff example

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	elliot := &Person{
+		Name: "Elliot",
+		Age:  24,
+		SocialFollowers: &SocialFollowers{
+			Youtube: 2500,
+			Twitter: 1400,
+		},
+	}
+
+	// data, err proto.Marshal(elliot)
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Fatal("marshalling error", err)
+	}
+	fmt.Println("Marshalling data", data)
+
+	// let's go the other way and unmarshal
+	// our protocol buffer into an object we can modify
+	// and use
+
+	newElliot := &Person{}
+	err = proto.Unmarshal(data, newElliot)
+	if err != nil {
+		log.Fatal("unmarshalling error", err)
+	}
+	fmt.Println("Unmarshalling data Name:", newElliot.GetName())
+	fmt.Println("Unmarshalling data Age:", newElliot.GetAge())
+	fmt.Println(newElliot.SocialFollowers.GetTwitter())
+	fmt.Println(newElliot.SocialFollowers.GetYoutube())
+
+}
